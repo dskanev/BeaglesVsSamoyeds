4bd55b9eda51c70968eac34923ed9edd5640d565
Denislav Kanev <denk@itu.dk>
2019-01-10 17:53:04 +0200

    Game works, win condition is recognized

--- app/src/main/java/denislav/kanev/lionortiger/MainActivity.java
+++ app/src/main/java/denislav/kanev/lionortiger/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     enum Player {
-        ONE, TWO
+        ONE, TWO, NONE
     }
 
     Player currentPlayer = Player.ONE;
@@ -17,12 +17,23 @@ public class MainActivity extends AppCompatActivity {
     Player[] playerChoices = new Player[9];
     int[][] winnerRowsColumns = {{0,1,2}, {3,4,5}, {6,7,8},
             {0,3,6},{1,4,7},{2,5,8},
-            {1,4,8}, {2,4,6}};
+            {0,4,8}, {2,4,6}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerChoices[0] = Player.NONE;
+        playerChoices[1] = Player.NONE;
+        playerChoices[2] = Player.NONE;
+        playerChoices[3] = Player.NONE;
+        playerChoices[4] = Player.NONE;
+        playerChoices[5] = Player.NONE;
+        playerChoices[6] = Player.NONE;
+        playerChoices[7] = Player.NONE;
+        playerChoices[8] = Player.NONE;
+
     }
 
     public void imageViewIsTapped(View imageView){
@@ -45,5 +56,13 @@ public class MainActivity extends AppCompatActivity {
 
         tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
         Toast.makeText(this, tappedImageView.getTag().toString(),Toast.LENGTH_SHORT).show();
+
+        for (int[] winnerColumns : winnerRowsColumns) {
+            if (playerChoices[winnerColumns[0]] == playerChoices[winnerColumns[1]]
+                    && playerChoices[winnerColumns[1]] == playerChoices[winnerColumns[2]]
+                    && playerChoices[winnerColumns[0]] != Player.NONE){
+                Toast.makeText(this, "We have a winner!", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
